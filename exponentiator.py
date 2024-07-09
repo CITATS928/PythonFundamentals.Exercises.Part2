5d1dab158a82eea152b2102d706d1455c648c797
CITATS928 <93238522+CITATS928@users.noreply.github.com>
2024-07-09 16:42:05 -0400

    Finished

--- exponentiator.py
+++ exponentiator.py
@@ -1,12 +1,16 @@
 #Exercise3
 def exponentiate(num1,num2):
+    """
+    This function is to raise the num1 to the num2 power
+    """
     return num1 ** num2
 
+"""power of 4"""
 def raise_to_fourth_power(num):
     return exponentiate(num,4)
 
-square = lambda x : x**2
-cube = lambda x:x**3
+square = lambda x : x**2 #power of 2
+cube = lambda x:x**3     #power of 3
 
 print(square(2))
 print(cube(2))
